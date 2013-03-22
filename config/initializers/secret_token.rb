e50731fc85a660d12d9c1b9d87c47442ca510470
johnrees <john@bitsushi.com>
2013-03-22 16:22:21 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bees::Application.config.secret_token = 'b9e35fbe06326a60db07506a5c40572982f2c195f4a0c086661127070653d18bc7f4305675320fc006655b38a342aeb58e1579000e6bad900b50ea42d5e53631'
